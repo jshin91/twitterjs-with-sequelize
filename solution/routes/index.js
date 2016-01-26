11b7890db938abe0a97b8875594908f9e24bb6a7
Jisoo Shin <jisooxshin@gmail.com>
2016-01-26 16:03:35 -0500

    solved except bonus

--- solution/routes/index.js
+++ solution/routes/index.js
@@ -10,11 +10,6 @@ module.exports = function makeRouterWithSockets (io) {
 
   // a reusable function
   function respondWithAllTweets (req, res, next){
-    
-    Tweet.findOne({include: [User]})
-    .then(function(tweets){
-      console.log('single tweet', JSON.stringify(tweets));
-    })
 
     // var allTheTweets = tweetBank.list();
     Tweet.findAll({include: [User]})
@@ -24,9 +19,7 @@ module.exports = function makeRouterWithSockets (io) {
       tweets: tweets,
       showForm: true
       });
-    });
-
-    
+    });    
   }
 
   // here we basically treet the root view and tweets view as identical
@@ -68,29 +61,43 @@ module.exports = function makeRouterWithSockets (io) {
     });
   });
 
+  function getUserId(name) {
+    return User.findOne({ where: {name: name} })
+  }
+
 
   // create a new tweet
   router.post('/tweets', function(req, res, next){
-    Tweet.upsert({
-      include: [User] {
-        name: req.body.name
-      },
-      tweet: req.body.text
-      userId: 
+    // console.log('req.body.name:', req.body.name);
+    getUserId(req.body.name)
+    .then(function(user){
 
-    }).then(function(tweet) {
-      io.sockets.emit('new_tweet', newTweet);
-      res.redirect('/');
+      if (user) return user;
+      else return User.create( {name: req.body.name})
+
+    })
+    .then(function(user){
+      Tweet.create({ 
+        tweet: req.body.text, 
+        UserId: user.id
+      })
+      .then(function(tweet){
+        io.sockets.emit('new_tweet', tweet);
+        res.redirect('/');
+      })
     })
   });
 
-
+  router.delete('/tweets/:id', function(req, res, next){
+    console.log('called delete', req.params.id);
+  });
   //to delete
   // router.post('/tweets', function(req, res, next){
   //   var newTweet = tweetBank.add(req.body.name, req.body.text);
   //   io.sockets.emit('new_tweet', newTweet);
   //   res.redirect('/');
   // });
+
 
 
   // // replaced this hard-coded route with general static routing in app.js
